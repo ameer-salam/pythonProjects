f56cf7ccea91bc380558c373894fe16856315f0d
Ameer Salam <ameer-salam@users.noreply.github.com>
2024-09-11 01:28:15 +0530

    Day 2

--- pythonProjects/Internship/Day 2/d2_q1.py
+++ pythonProjects/Internship/Day 2/d2_q1.py
@@ -1,1 +1,13 @@
 #initialize array with 5 no and add new no to the array and print the updated array
+arraY=[1,2,3,4,5]
+
+#adding by append
+arraY.append(1)
+for i in arraY:
+    print(i)
+print("End!\n\n")
+
+#adding by replacement
+arraY[2] = 6
+for i in arraY:
+    print(i)
