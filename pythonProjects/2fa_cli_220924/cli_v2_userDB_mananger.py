d2801e546a285fa30f31484fb7657748920956f0
Ameer Salam <ameer-salam@users.noreply.github.com>
2024-09-25 21:50:22 +0530

    updates

--- pythonProjects/2fa_cli_220924/cli_v2_userDB_mananger.py
+++ pythonProjects/2fa_cli_220924/cli_v2_userDB_mananger.py
@@ -8,4 +8,24 @@ class users_list(Base):
     __tablename__ = "users"
 
     user_no=Column("user_no", Integer, primary_key=True)
-    user_name=Column("user_name", String, primary_key=False)
+    user_name=Column("user_names", String, nullable=False)
+    user_keys_db=Column("user_keys_path", String, nullable=False)
+
+    def __init__(self, user_no, user_names, user_keys_db):
+        self.user_no = user_no
+        self.user_names = user_names
+        self.user_keys_db = user_keys_db
+
+    #def _repr__(self):
+
+engine = create_engine("sqlite:///users_list.db", echo=True) #connects the Database
+
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+user=users_list(1, "Ameer", "KEys_exAmPle1")
+session.add(user)
+session.commit()
+print(user)
